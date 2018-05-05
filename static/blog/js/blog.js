9b429b3de8194f541373cb2b405555bb4ca4ef6c
Aly-ve <alysson.hussin@gmail.com>
2018-05-06 01:37:50 +0200

    [Update] Static files
    * Add Mastodon-Share-Button
    * Update Bootstrap to 4.1.1
    * Move fortawesome to forkawesome

--- static/blog/js/blog.js
+++ static/blog/js/blog.js
@@ -1,19 +1,9 @@
-$(document).ready(function() {
-    
-    $(window).scroll(function() {
-        if($(window).scrollTop() >= 60) {
-            $('a.navbar-brand').css('font-size', '1.5em');
-        }
-        else {
-            $('a.navbar-brand').css('font-size', '3em');
-        }
-    })
-    
+(function() {
     try {
-        $('#modal-hidden-image').modal('show');
+        document.querySelector('#modal-hidden-image').modal('show');
 
         document.querySelector('#hidden-image-display-all').addEventListener('click', function() {
-            $('#modal-hidden-image').modal('hide');
+            document.querySelector('#modal-hidden-image').modal('hide');
             document.querySelectorAll('.hidden img').forEach(function(e) {
                 e.style.display = "block"
             })
